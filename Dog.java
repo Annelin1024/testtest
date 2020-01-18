df06a1f1fdf1adc9513e24485e6e288fa90cf68e
Anne Lin <annefunnyday99@gmail.com>
2020-01-18 14:11:51 +0800

    test commit

---
+++ Dog.java
@@ -0,0 +1,39 @@
+
+public class Dog implements getWeightMethod{
+	private String name;
+	private double weight;
+	
+	public Dog() {
+		super();
+	}
+
+	public Dog(String name, double weight) {
+		super();
+		this.name = name;
+		this.weight = weight;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	@Override
+	public String getWeightmethod() {
+		return "體重計";
+//		System.out.print("體重計");
+	}
+	
+	
+}
